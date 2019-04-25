c0f33dae76e60db06f7ee6556c5adafaae5b56ed
unflyingKat <screenfilleddesk@gmail.com>
2019-04-25 13:05:06 +0200

    Fixes

--- Opp_2/src/no/hvl/dat102/Klient.java
+++ Opp_2/src/no/hvl/dat102/Klient.java
@@ -4,25 +4,25 @@ import java.util.*;
 import java.util.Random;
 
 public class Klient {
+	static Random terning = new Random();
 
 	/**
 	 * @param args
 	 */
 
-	private void testRun() {
-
-		Random terning = new Random();
+	private static KjedetBSTre<Integer> setup() {
 		KjedetBSTre<Integer> bstre = new KjedetBSTre<Integer>();
 
 		for (int i = 0; i < 1024; i++) {
 			bstre.leggTil(terning.nextInt());
 		}
-		
-
+		return bstre;
 	}
 
 	public static void main(String[] args) {
-		KjedetBSTre<Integer> bstre = new KjedetBSTre<Integer>();
+		terning.nextInt();
+		KjedetBSTre<Integer> bstre = setup();
+		System.out.println(bstre.antall());
 		
 	}
 
