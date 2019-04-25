dc53948968cbc2e3a1f5a8142aa78be1d9e6d0d5
unflyingKat <screenfilleddesk@gmail.com>
2019-04-25 12:33:01 +0200

    test

--- Opp_2/src/no/hvl/dat102/Klient.java
+++ Opp_2/src/no/hvl/dat102/Klient.java
@@ -1,11 +1,26 @@
 package no.hvl.dat102;
 
+import java.util.Random;
+
 public class Klient {
 
 	/**
 	 * @param args
 	 */
+
+	private void testRun() {
+
+		Random terning = new Random();
+		KjedetBSTre<Integer> bstre = new KjedetBSTre<Integer>();
+
+		for (int i = 0; i < 1024; i++) {
+			bstre.leggTil(terning.nextInt());
+		}
+
+	}
+
 	public static void main(String[] args) {
+
 		KjedetBSTre bstre = new KjedetBSTre();
 
 		bstre.leggTil(7);
@@ -41,5 +56,4 @@ public class Klient {
 		}
 	}
 
-}
 }
